6ca0a78004eb37241d9ff55efa56ef2af1651fce
Student <student@localhost.localdomain>
2021-11-18 15:18:12 -0500

    Starting Packet Parser

--- packet_parser.py
+++ packet_parser.py
@@ -1,4 +1,14 @@
-def parse() :
-	print('called parse function in packet_parser.py')
+class Packet:
+    def __init__(self):
+        self.ip_hdr_len = 0
 
-	
+def parse(file_name) :
+	f = open(file_name)
+        lines = f.readlines()
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+            while line != "":
+                split_hex = line.split()
+                i += 1
+
