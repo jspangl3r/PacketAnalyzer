1d78a461fa59245b287a811ec835d4becba9088e
cGhatt <94644325+cGhatt@users.noreply.github.com>
2021-11-30 15:13:40 -0500

    Add files via upload
    final version of filter_packets.py

--- filter_packets.py
+++ filter_packets.py
@@ -1,34 +1,39 @@
 import sys
 # Project 2
-# Filte_packets.py by caleb ghatt (trying his best!)
+# filter_packets.py by Caleb Ghatt and Gang 
+
 
 def filter(fileName) :
 	print('called filter function in filter_packets.py')
 	f=open(str(fileName), 'r')
 	line=f.readline()
 	fileName=fileName[:-4]
+	# Opens and wipes previous versions of file
+	open(fileName+"_filtered.txt", "w").close()
 	while line:
-		#line=f.readline()
 		line=line.strip()
+		# Only reads in lines with Echo Requests/Replies in packet header
 		if "Echo" in line:
-			print("", file=open(fileName+"_filtered.txt", "a"))
+			
 			print("No.     Time           Source                Destination           Protocol Length Info", file=open(fileName+"_filtered.txt", "a"))
 			
 			print(line, file=open(fileName+"_filtered.txt", "a"))
 			line=f.readline().strip()
 
+			# checks the next line
 			if line=="":
-				#line=f.readline() #Commenting this out will determine whether the program skips the line or adds blank
+				# While line isnt empty reads HEX code
 				while(line!="\n"):
 					print(str.rstrip(line), file=open(fileName+"_filtered.txt", "a"))
 					line=f.readline()
 
-			#line=f.readline()
 			line=f.readline()
-			#print(line)
+			# prints blank line between readings
+			print("", file=open(fileName+"_filtered.txt", "a"))
 		line=f.readline()
 	f.close()
 
+# Get User Text File
 fileName=str(sys.argv[1])
 filter(fileName)
 
