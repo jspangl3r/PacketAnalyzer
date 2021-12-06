0b0c3aa361c02436d346ce4d151c1cbb92b538b0
jspangled <jes3277@rit.edu>
2021-12-06 14:31:45 -0500

    Hopefully finish metrics and output

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,12 +1,11 @@
 from packet_parser import Packet
+import math
 
 # Global IP information of nodes
 NODE_IPS = ["192.168.100.1", "192.168.100.2", "192.168.200.1", "192.168.200.2"]
 
 def compute(packets, node_num, output_f):
     node_ip = NODE_IPS[node_num - 1]
-    
-    output_f.write(f"Node {node_num}\n\n")
 
     # Data size metrics
     echo_requests_sent = [p for p in packets if p.src_ip == node_ip and p.type == 8]
@@ -30,22 +29,47 @@ def compute(packets, node_num, output_f):
     # 8. Total Echo Request data received
     total_echo_request_data_received = sum([p.data_len for p in echo_requests_received])
     
+    # Time Based Metrics
+    # 1. Average Ping Round Trip Time (RTT)
+    ping_rtts = []
+    for request in echo_requests_sent:
+        # Find corresponding ping reply
+        reply = [p for p in echo_replies_received if p.src_ip == request.dest_ip and
+                 p.dest_ip == request.src_ip and p.seq_num == request.seq_num][0]
+        ping_rtts.append((reply.time - request.time)*1000) 
+    avg_rtt = sum(ping_rtts) / len(ping_rtts)
+    # 2. Echo Request Throughput
+    echo_request_throughput = sum([p.frame_len for p in echo_requests_sent]) / sum(ping_rtts) 
+    # 3. Echo Request Goodput
+    echo_request_goodput = sum([p.data_len for p in echo_requests_sent]) / sum(ping_rtts)
+    # 4. Average Reply Delay
+    avg_reply_delay = 0
+    for request in echo_requests_received:
+        # Find corresponding ping reply
+        reply = [p for p in echo_replies_sent if p.src_ip == request.dest_ip and
+                 p.dest_ip == request.src_ip and p.seq_num == request.seq_num][0]
+        avg_reply_delay += (reply.time - request.time)*1000000
+    avg_reply_delay /= num_echo_requests_received    
+ 
+   # Distance Metric - average num hops per echo request
+    avg_hops = 0
+    for p in echo_requests_sent:
+        # Check if source and dest IPs are on the same network
+        src_ip_split = p.src_ip.split(".")
+        dest_ip_split = p.dest_ip.split(".")
+        avg_hops += 1 if src_ip_split[2] == dest_ip_split[2] else 3
+    avg_hops /= num_echo_requests_sent
+    
+    # Write metrics to output file
+    output_f.write(f"Node {node_num}\n\n")
     output_f.write("Echo Requests Sent,Echo Requests Received,Echo Replies Sent,Echo Replies Received\n")
-    output_f.write(f"{num_echo_requests_sent},{num_echo_requests_received},{num_echo_replies_sent},{num_echo_replies_received}\n\n")
+    output_f.write(f"{num_echo_requests_sent},{num_echo_requests_received},{num_echo_replies_sent},{num_echo_replies_received}\n")
     output_f.write("Echo Request Bytes Sent (bytes),Echo Request Data Sent (bytes)\n")
     output_f.write(f"{total_echo_request_bytes_sent},{total_echo_request_data_sent}\n")
     output_f.write("Echo Request Bytes Received (bytes),Echo Request Data Received (bytes)\n")
     output_f.write(f"{total_echo_request_bytes_received},{total_echo_request_data_received}\n\n")
-     
-    # Time Based Metrics
-    # 1. Average Ping Round Trip Time (RTT)
-    ping_rtts = []
-    avg_rtt = 0
-    for request in echo_requests_sent:
-        # Find corresponding ping reply
-        reply = [p for p in echo_replies_received if p.seq_num == request.seq_num][0]
-        ping_rtts.append((reply.time - request.time)*1000) 
-    avg_rtt = sum(ping_rtts) / len(ping_rtts)
-    # 2. 
-    
-    output_f.write(f"Average RTT (milliseconds),{avg_rtt}\n")
+    output_f.write(f"Average RTT (milliseconds),{avg_rtt:.2f}\n")
+    output_f.write(f"Echo Request Throughput (kB/sec),{echo_request_throughput:.1f}\n")
+    output_f.write(f"Echo Request Goodput (kB/sec),{echo_request_goodput:.1f}\n")
+    output_f.write(f"Average Reply Delay (microseconds),{avg_reply_delay:.2f}\n") 
+    output_f.write(f"Average Echo Request Hop Count,{avg_hops:.2f}\n") 
